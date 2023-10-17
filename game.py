932835ad490958619998e47e1bd52130752f91fd
boppabopp <148220992+boppabopp@users.noreply.github.com>
2023-10-17 09:27:53 -0400

    Update game.py

--- game.py
+++ game.py
@@ -8,6 +8,9 @@ HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 background = pygame.Surface((WIDTH, HEIGHT))
 
+puffer_fish = pygame.image.load('images\puffer_fish.png')
+
+
 background.fill((52, 140, 235))
 sand_height = 100
 pygame.draw.rect(background, (250, 192, 85), (0, HEIGHT-sand_height, WIDTH, sand_height))
@@ -19,4 +22,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+    screen.blit(puffer_fish, (400,300))
+
     pygame.display.flip()
+
